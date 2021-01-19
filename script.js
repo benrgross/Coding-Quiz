251edb51362218f565dd32798b7716ce14c48d38
Ben Gross <benrgross@gmail.com>
2021-01-18 19:21:54 -0500

    added a highscore card, and wrote a function to add initials to a list.

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 var questions = [
   {
-    question: "What is 2 + 2 ",
-    answers: ["A: 44", "B: 4 ", "C: 22"],
-    correct: "B: 4",
+    question:
+      "What is the HTML tag under which one can write the JavaScript code?",
+    answers: ["A) <javascript>", "B) <scripted>", "C) <script>", "D) <js>"],
+    correct: "C: <script>",
   },
   {
-    question: "what is a buttfor?",
-    answers: ["A: a tool", "B: a pickle", "C: pooping silly"],
-    correct: "C: pooping silly",
+    question: "Which built-in method returns the length of the string?",
+    answers: [
+      "A) length()",
+      "B) size()",
+      "C)index()",
+      "D) D - None of the above.",
+    ],
+    correct: "B) document.getElementById(“geek”).innerHTML=”I am a Geek”;",
+  },
+  {
+    question:
+      "Which of the following is the correct syntax to display “Hello” in an alert box?",
+    answers: [
+      "A. alertbox(“Hello”);",
+      "B. msg(“Hello”);",
+      "C. msgbox(“Hello”);",
+      "D. alert(“Hello”);",
+    ],
+    correct: "D. alert(“Hello”);",
+  },
+  {
+    question:
+      "4. What is the correct syntax for referring to an external script called “geek.js”?",
+    answers: [
+      "A. <script src=”geek.js”>",
+      "B. <script href=”geek.js”>",
+      "C. <script ref=”geek.js”>",
+      "D. <script name=”geek.js”>",
+    ],
+    correct: "A. <script src=”geek.js”>",
   },
 ];
 
@@ -20,17 +48,24 @@ var score = 0;
 
 correct = questions[questionIndex].correct;
 
-// start quiz function when start button clicked
+// get elements
 var startBtn = document.getElementById("start");
-startBtn.addEventListener("click", startGame);
 var startContainer = document.getElementById("start-container");
 var showQuestions = document.getElementById("quiz-box");
 var questionEl = document.getElementById("question");
 var answerElement = document.getElementById("answers");
 var timerEl = document.getElementById("display-time");
 var validateAnswer = document.getElementById("CorrectIncorrect");
+var initialsInput = document.querySelector('input[type="text"]');
+var form = document.querySelector("form");
+var scoreSubmit = document.getElementById("score-card");
+var highScores = document.getElementById("highscore");
+var playAgainBtn = document.getElementById("play-again");
 
-/// get elements by id
+//add event listeners
+startBtn.addEventListener("click", startGame);
+form.addEventListener("submit", submitHighscore);
+playAgainBtn.addEventListener("click", playAgain);
 
 // display questions in quiz content
 
@@ -66,6 +101,7 @@ function displayQuestions(questions) {
   }
 }
 
+// logs the content string of button clicked to be compares to correct string
 function buttonClick(e) {
   console.log(e.target);
   var answer = e.target.innerText;
@@ -73,15 +109,21 @@ function buttonClick(e) {
   Validate(answer, correct);
   if (questionIndex < questions.length) {
     selectQuestion();
+  } else {
+    showQuestions.classList.add("hide");
+    scoreSubmit.classList.remove("hide");
+    // stop timer
   }
 }
 
+// resets page so new question card can be shown.
 function resetState() {
   while (answerElement.firstChild) {
     answerElement.removeChild(answerElement.firstChild);
   }
 }
 
+//compares answer to correct answer and adds score and goes to next question or stops game
 function Validate(answer, correct) {
   correct = "";
   correct = questions.correct;
@@ -90,12 +132,11 @@ function Validate(answer, correct) {
     score++;
     console.log(score);
   }
+  // else {
+  //   timeLeft - 5;
+  // }
 }
 
-//add event listenter that stores answer
-//resets state adds question index ++ and goes to next question
-//need if statement that validates answer and moves on to the next
-//store score somewhere
 //set timer function
 function timer() {
   var timeLeft = 90;
@@ -105,8 +146,31 @@ function timer() {
     //need if statement if(answer false time - 10)
     if (timeLeft === 0) {
       clearInterval(timeInterval);
+      showQuestions.classList.add("hide");
+      scoreSubmit.classList.remove("hide");
     }
   }, 500);
+}
+
+//record initials from form and trigger next event
+function submitHighscore(e) {
+  e.preventDefault();
+  //get input value
+  var logInitials = document.getElementById("initials").value;
+  // create new li element
+  var li = document.createElement("li");
+  li.className = "score-item";
+  console.log(li);
+  // add text node with input value
+  li.appendChild(document.createTextNode(logInitials));
+  highScores.appendChild(li);
+  scoreSubmit.classList.add("hide");
+  highScores.classList.remove("hide");
+}
+
+function playAgain(e) {
+  highScores.classList.add("hide");
+  startContainer.classList.remove("hide");
 }
 
 // qBtn.addEventListener("click", function (answer, correct) {
